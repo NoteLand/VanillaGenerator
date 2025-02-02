5ed1d9726c3d4e27b8eb2c40c0e97960e8822617
Crasher508 <45127993+Crasher508@users.noreply.github.com>
2025-02-02 11:35:50 +0100

    Add gold ore and ancient debris to nether generation

--- src/muqsit/vanillagenerator/generator/nether/populator/OrePopulator.php
+++ src/muqsit/vanillagenerator/generator/nether/populator/OrePopulator.php
@@ -18,7 +18,9 @@ class OrePopulator extends OverworldOrePopulator{
 	 * @param int $world_height
 	 */
 	public function __construct(int $world_height = World::Y_MAX){
+		$this->addOre(new OreType(VanillaBlocks::NETHER_GOLD_ORE(), 10, 117, 16, BlockTypeIds::NETHERRACK), 10);
 		$this->addOre(new OreType(VanillaBlocks::NETHER_QUARTZ_ORE(), 10, $world_height - (10 * ($world_height >> 7)), 13, BlockTypeIds::NETHERRACK), 16);
 		$this->addOre(new OreType(VanillaBlocks::MAGMA(), 26, 32 + (5 * ($world_height >> 7)), 32, BlockTypeIds::NETHERRACK), 16);
+		$this->addOre(new OreType(VanillaBlocks::ANCIENT_DEBRIS(), 8, 22, 2, BlockTypeIds::NETHERRACK), 1);
 	}
 }
